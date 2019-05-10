c5ac52b86f427e92572579afac2cb85ddd659110
Serhii Merkulov <finusrom@gmail.com>
2019-05-10 11:12:20 +0300

    added test case

--- PUNCH/lab2/main.py
+++ PUNCH/lab2/main.py
@@ -1,5 +1,6 @@
 import threading
 import math
+import time
 
 results = []
 
@@ -9,6 +10,8 @@ def f(x):
 
 def g(x):
     print('g')
+    while True:
+        foo = True
     results.append(int(x) * int(x))
 
 def checkResults():
@@ -20,14 +23,23 @@ def main():
     t1 = threading.Thread(target=f, args=(x,))
     t2 = threading.Thread(target=g, args=(x,))
 
+    startTime = time.time()
+
     t1.start()
     t2.start()
 
-    while not checkResults():
-        print('running...')
+    while not checkResults() and time.time() - startTime < 10:
+        needBreak = False
+        for x in results:
+            if x == 0:
+                needBreak = True
+        if needBreak:
+            break
 
     print('DONE\n\n')
-    result = results[0] * results[1]
+    result = 1
+    for x in results:
+        result *= x
     print(result)
 
 if __name__ == '__main__': main()
