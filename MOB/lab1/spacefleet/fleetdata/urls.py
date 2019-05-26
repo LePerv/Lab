7b2c590bc6c9609cdabf70bfc1bd63d454223732
Serhii Merkulov <finusrom@gmail.com>
2019-05-26 14:47:07 +0300

    added navigation and views for all tables

--- MOB/lab1/spacefleet/fleetdata/urls.py
+++ MOB/lab1/spacefleet/fleetdata/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('race/', views.race, name='race'),
+    path('pilot/', views.pilot, name='pilot'),
+    path('starship/', views.starship, name='starship'),
+    path('station/', views.station, name='station'),
+    path('fleet/', views.fleet, name='fleet'),
 ]
--- MOB/lab1/spacefleet/fleetdata/views.py
+++ MOB/lab1/spacefleet/fleetdata/views.py
@@ -2,12 +2,54 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 from fleetdata.models import Race
+from fleetdata.models import Pilot
+from fleetdata.models import Starship
+from fleetdata.models import Station
+from fleetdata.models import Fleet
 
 
 def index(request):
-    races = Race.objects.all()
     template = loader.get_template('fleetdata/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def race(request):
+    races = Race.objects.all()
+    template = loader.get_template('fleetdata/race.html')
     context = {
         'race_list': races,
     }
     return HttpResponse(template.render(context, request))
+
+def pilot(request):
+    pilots = Pilot.objects.all()
+    template = loader.get_template('fleetdata/pilot.html')
+    context = {
+        'pilot_list': pilots,
+    }
+    return HttpResponse(template.render(context, request))
+    
+def starship(request):
+    starships = Starship.objects.all()
+    template = loader.get_template('fleetdata/starship.html')
+    context = {
+        'starship_list': starships,
+    }
+    return HttpResponse(template.render(context, request))
+    
+def station(request):
+    stations = Station.objects.all()
+    template = loader.get_template('fleetdata/station.html')
+    context = {
+        'station_list': stations,
+    }
+    return HttpResponse(template.render(context, request))
+
+def fleet(request):
+    fleets = Fleet.objects.all()
+    template = loader.get_template('fleetdata/fleet.html')
+    context = {
+        'fleet_list': fleets,
+    }
+    return HttpResponse(template.render(context, request))
+
