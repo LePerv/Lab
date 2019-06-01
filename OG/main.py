9045c01050edcf0a615eb5c1f844f538acf4542f
Serhii Merkulov <finusrom@gmail.com>
2019-06-01 17:11:41 +0300

    finished convex hull builder

--- OG/main.py
+++ OG/main.py
@@ -5,30 +5,32 @@ import matplotlib.pyplot as plt
 class ListNode:
     def __init__(self, value):
         self.value = value
-        self.left = None
-        self.right = None
-        pass
+        self.next = None
+        self.prev = None
+
     
 class LinkedList:
     def __init__(self, array):
         self.start = None
         if array:
-            while len(array) > 0:
-                x = array.pop()
+            _array = array[:]
+            _array.reverse()
+            while len(_array) > 0:
+                x = _array.pop()
                 node = ListNode(x)
                 self.add(node)
     
     def add(self, node):
         if self.start:
-            last = self.start.right
-            last.left = node
-            node.right = last
-            node.left = self.start
-            self.start.right = node
+            last = self.start.prev
+            last.next = node
+            node.prev = last
+            node.next = self.start
+            self.start.prev = node
         else:
             self.start = node
-            self.start.left = node
-            self.start.right = node
+            self.start.next = node
+            self.start.prev = node
 
     def remove(self, node):
         _node = None
@@ -36,14 +38,14 @@ class LinkedList:
             if _node is None:
                 _node = self.start
             if _node == node:
-                l = _node.left
-                r = _node.right
-                l.right = r
-                r.left = l
+                n = _node.next
+                p = _node.prev
+                n.prev = p
+                p.next = n
                 if node == self.start:
-                    self.start = l
+                    self.start = n
                 break
-            _node = _node.left
+            _node = _node.next
 
     def length(self):
         result = 0
@@ -52,160 +54,173 @@ class LinkedList:
             if node is None:
                 node = self.start
             result += 1
-            node = node.left
+            node = node.next
         return result
 
-class Point:
+    def to_array(self):
+        node = None
+        arr = []
+        while node != self.start:
+            if node is None:
+                node = self.start
+            arr.append(node.value)
+            node = node.next
+        return arr
+
+
+class Vec2:
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        if x is None:
+            self.x = 0
+        else:
+            self.x = x
+        if y is None:
+            self.y = 0
+        else:
+            self.y = y
 
-class Triangle:
-    def __init__(self, v1, v2, v3):
-        self.v1 = v1
-        self.v2 = v2
-        self.v3 = v3
-
-def calculateAngle(left, middle, right):
-    v1 = Point(left.x - middle.x, left.y - middle.y)
-    v2 = Point(middle.x - right.x, middle.y - right.y)
+    def __str__(self):
+        return "( " + str(self.x) + "; " + str(self.y) + " )" 
     
-    return math.acos((v1.x * v2.x + v1.y * v2.y) / (math.sqrt(math.pow(v1.x, 2) + math.pow(v1.y, 2)) * math.sqrt(math.pow(v2.x, 2) + math.pow(v2.y, 2))))
+    def add(self, vec2):
+        return Vec2(self.x + vec2.x, self.y + vec2.y)
 
-def calculateTriangleSquare(t1, t2, t3):
-    a = math.sqrt(math.pow(t1.x - t2.x, 2) + math.pow(t1.y - t2.y, 2))
-    b = math.sqrt(math.pow(t2.x - t3.x, 2) + math.pow(t2.y - t3.y, 2))
-    c = math.sqrt(math.pow(t1.x - t3.x, 2) + math.pow(t1.y - t3.y, 2))
-    hp = (a + b + c) / 2
+    def sub(self, vec2):
+        return Vec2(self.x - vec2.x, self.y - vec2.y)
 
-    return math.sqrt(hp * (hp - a) * (hp - b) * (hp - c))
+    def mult(self, vec2):
+        return Vec2(self.x * vec2.x, self.y * vec2.y)
 
-def isPointInTriangle(p, t1, t2, t3):
-    s = calculateTriangleSquare(t1, t2, t3)
-    s1 = calculateTriangleSquare(p, t1, t2)
-    s2 = calculateTriangleSquare(p, t2, t3)
-    s3 = calculateTriangleSquare(p, t3, t1)
-    
-    return math.fabs(s - (s1 + s2 + s3)) < 1e-9
+    def div(self, vec2):
+        return Vec2(self.x / vec2.x, self.y / vec2.y)
 
-def drawPolygon(points):
+    def distance(self, vec2):
+        temp = self.sub(vec2)
+        return math.sqrt(temp.x * temp.x + temp.y * temp.y)
+
+    def dot(self, vec2):
+        return self.x * vec2.x + self.y * vec2.y
+
+    def dup(self):
+        return Vec2(self.x, self.y)
+
+
+def calculate_angle(p1, p2, p3):
+    return math.acos(p1.sub(p2).dot(p3.sub(p2)) / (p1.distance(p2) * p3.distance(p2)))
+
+def is_right_turn(p1, p2, p3):
+    delta = p1.x * p2.y - p1.x * p3.y - p2.x * p1.y + p3.x * p1.y + p2.x * p3.y - p3.x * p2.y
+    return delta > 0
+
+def is_left_turn(p1, p2, p3):
+    delta = p1.x * p2.y - p1.x * p3.y - p2.x * p1.y + p3.x * p1.y + p2.x * p3.y - p3.x * p2.y
+    return delta < 0
+
+def find_inner_point(points):
     x = []
     y = []
     for point in points:
         x.append(point.x)
         y.append(point.y)
-    x = np.array(x)
-    y = np.array(y)
-    plt.figure()
-    plt.fill(x, y, facecolor='none', edgecolor='black', linewidth=1)
-    plt.show()
+    center_x = (max(x) + min(x)) / 2
+    center_y = (max(y) + min(y)) / 2
+    return Vec2(center_x, center_y)
 
-def drawTriangles(triangles):
-    plt.figure()
-    for triangle in triangles:
-        x = np.array([triangle.v1.value[0].x, triangle.v2.value[0].x, triangle.v3.value[0].x])
-        y = np.array([triangle.v1.value[0].y, triangle.v2.value[0].y, triangle.v3.value[0].y])
-        plt.fill(x, y, facecolor='none', edgecolor='black', linewidth=1)
-    plt.show()
+def prepare_points(points, center):
+    helper = center.dup().add(Vec2(1, 0))
+    result = []
+    angles = []
+    temp = {}
+
+    for point in points:
+        current_angle = calculate_angle(point, center, helper)
+        if point.y < center.y:
+            current_angle = 2 * math.pi - current_angle
+        angles.append(current_angle)
+        temp[current_angle] = point
+
+    angles.sort()
+    for angle in angles:
+        result.append(temp[angle])
+
+    return result
+
+def build_convex_hull(points):
+    pts = LinkedList(points)
+
+    min_y = points[0].y
+    min_point = points[0]
+    for point in points:
+        if point.y < min_y:
+            min_y = point.y
+            min_point = point
+        elif point.y == min_y:
+            if point.x < min_point.x:
+                min_point = point
+
+    start = None
+    node = None
+    while node != pts.start:
+        if node is None:
+            node = pts.start
+        if node.value == min_point:
+            start = node
+            break
+        node = node.next
+
+    f = False
+    v = start
+    w = v.prev
+    while v.next != start or not f:
+        if v.next == w:
+            f = True
+        p1 = v.value
+        p2 = v.next.value
+        p3 = v.next.next.value
+        if is_right_turn(p1, p2, p3):
+            v = v.next
+        else:
+            pts.remove(v.next)
+            v = v.prev
+    pts.add(ListNode(pts.start.value))
+    return pts.to_array()
 
 def main():
-    temp = open('input.txt', 'r')
+    lines = open('input.txt', 'r')
+    points = LinkedList([])
+    for line in lines:
+        values = line.split(' ')
+        x = float(values[0])
+        y = float(values[1])
+        points.add(ListNode(Vec2(x, y)))
 
-    points = []
-    for line in temp:
-        xy = line.split(' ')
-        points.append(Point(float(xy[0]), float(xy[1])))
-    points = LinkedList(points)
-    angles = []
-    node = None
-    while node != points.start:
-        if node is None:
-            node = points.start
-        angles.append(calculateAngle(node.left.value, node.value, node.right.value))
-        node = node.left
+    q_point = find_inner_point(points.to_array())
+    ordered = prepare_points(points.to_array(), q_point)
 
-    vertices = []
-    node = None
-    while node != points.start:
-        if node is None:
-            node = points.start
-        vertices.append([node.value, angles.pop()])
-        node = node.left
-    vertices = LinkedList(vertices)
+    convex_hull = build_convex_hull(ordered)
+    print('\n\n\n')
+    for point in convex_hull:
+        print(point)
 
-    result_triangles = []
-    work = vertices.start
+    x_points = []
+    y_points = []
+    x_convex_hull = []
+    y_convex_hull = []
+    for point in points.to_array():
+        x_points.append(point.x)
+        y_points.append(point.y)
+    for point in convex_hull:
+        x_convex_hull.append(point.x)
+        y_convex_hull.append(point.y)
 
-    while vertices.length() > 2:
-        temp_triangles = []
-        left = work.left
-        right = work.right
+    x_points = np.array(x_points)
+    y_points = np.array(y_points)
+    x_convex_hull = np.array(x_convex_hull)
+    y_convex_hull = np.array(y_convex_hull)
 
-        #fisrt check
-        v11 = work
-        v12 = left
-        v13 = left.left
-        node1 = left.left.left
-        check1 = True
-        while node1 != work:
-            if isPointInTriangle(node1.value[0], v11.value[0], v12.value[0], v13.value[0]):
-                check1 = False
-                break
-            node1 = node1.left
-        if left.value[1] < math.pi and check1:
-            temp_triangles.append(Triangle(v11, v12, v13))
-
-        #second check
-        v21 = work
-        v22 = right
-        v23 = right.right
-        node2 = right.right.right
-        check2 = True
-        while node2 != work:
-            if isPointInTriangle(node2.value[0], v21.value[0], v22.value[0], v23.value[0]):
-                check2 = False
-                break
-            node2 = node2.right
-        if right.value[1] < math.pi and check2:
-            temp_triangles.append(Triangle(v21, v22, v23))
-
-        #third check
-        v31 = work
-        v32 = left
-        v33 = right
-        node3 = left.left
-        check3 = True
-        while node3 != right:
-            if isPointInTriangle(node3.value[0], v31.value[0], v32.value[0], v33.value[0]):
-                check3 = False
-                break
-            node3 = node3.right
-        if work.value[1] < math.pi and check3:
-            temp_triangles.append(Triangle(v31, v32, v33))
-
-        if len(temp_triangles) == 0:
-            work = work.left
-        else:
-            min_delta_angle = None
-            min_triangle = None
-            for x in temp_triangles:
-                a1 = calculateAngle(x.v1.value[0], x.v2.value[0], x.v3.value[0])
-                a2 = calculateAngle(x.v2.value[0], x.v3.value[0], x.v1.value[0])
-                a3 = calculateAngle(x.v3.value[0], x.v1.value[0], x.v2.value[0])
-                min_a = min([a1, a2, a3])
-                max_a = max([a1, a2, a3])
-                delta = max_a - min_a
-                if min_delta_angle is None or delta < min_delta_angle:
-                    min_delta_angle = delta
-                    min_triangle = x
-            result_triangles.append(min_triangle)
-            trash = min_triangle.v2
-            vertices.remove(trash)
-            trash.left.value[1] = calculateAngle(left.left.value[0], left.value[0], right.value[0])
-            trash.right.value[1] = calculateAngle(left.value[0], right.value[0], right.right.value[0])
-            work = trash.left
-    
-    drawTriangles(result_triangles)
+    plt.plot(x_points, y_points, 'o')
+    plt.plot(x_convex_hull, y_convex_hull)
+    plt.show()
 
 
 if __name__ == '__main__':
